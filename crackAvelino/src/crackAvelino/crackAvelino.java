b841f0d29ad31121061366198fccf2403ab2c0a3
Vinicius Gubiani Ferreira <vini.g.fer@gmail.com>
2019-09-26 23:09:22 -0300

    feat: Cracked cipher texts

--- crackAvelino/src/crackAvelino/crackAvelino.java
+++ crackAvelino/src/crackAvelino/crackAvelino.java
@@ -21,24 +21,43 @@ public class crackAvelino {
 	}
 	
 	public static SecretKeySpec getSecretKey(String passwd) throws Exception {
-//	    byte[] dataBytes = passwd.getBytes();
-//	    MessageDigest md = MessageDigest.getInstance("SHA-256");
-//	    md.update(dataBytes, 0, passwd.length());
-//	    byte[] mdbytes = md.digest();
-		byte[] mdbytes = toByteArray("36f18357be4dbd77f050515c73fcf9f2");
-	    return new SecretKeySpec(Arrays.copyOfRange(mdbytes, 0, 16), "AES");
+        //byte[] dataBytes = passwd.getBytes();
+        byte[] dataBytes = toByteArray(passwd);
+
+        // MessageDigest md = MessageDigest.getInstance("SHA-256");
+        // md.update(dataBytes, 0, passwd.length());
+        // byte[] mdbytes = md.digest();
+
+        //return new SecretKeySpec(Arrays.copyOfRange(mdbytes, 0, 16), "AES");
+        return new SecretKeySpec(dataBytes, "AES");
 	}
 	
 	public static void main(String[] args) throws Exception {
-		String chave = "abcdefghi";
-		String textoCifrado = "69dda8455c7dd4254bf353b773304eec0ec7702330098ce7f7520d1cbbb20fc388d1b0adb5054dbd7370849dbf0b88d393f252e764f1f5f7ad97ef79d59ce29f5f51eeca32eabedd9afa9329";
+		String chave = "36f18357be4dbd77f050515c73fcf9f2";
+		String textoCifrado = "770b80259ec33beb2561358a9f2dc617e46218c0a53cbeca695ae45faa8952aa0e311bde9d4e01726d3184c34451";
 		
-		SecretKeySpec skeySpec = getSecretKey(chave);
-	    Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
-	    IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
-	    cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
-	    byte[] deciphered = cipher.doFinal(toByteArray(textoCifrado));
-	    System.out.println("Mensagem decifrada: " + (new String(deciphered)));
+//		SecretKeySpec skeySpec = getSecretKey(chave);
+//	    Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
+//	    IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+//	    cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+//	    byte[] deciphered = cipher.doFinal(toByteArray(textoCifrado));
+//	    System.out.println("Mensagem decifrada: " + (new String(deciphered)));
+	    
+	    
+        SecretKeySpec skeySpec = getSecretKey(chave);
+
+        String input = textoCifrado;
+        input = input.substring(0, 32);
+        byte[] iv = toByteArray(input);
+        
+        IvParameterSpec ivp = new IvParameterSpec(iv);
+
+        //Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
+        cipher.init(Cipher.DECRYPT_MODE, skeySpec, ivp);
+
+        byte[] deciphered = cipher.doFinal(toByteArray(textoCifrado.substring(32)));
+        System.out.println("Mensagem decifrada: " + new String(deciphered));
 	}
 
 }
